5a6e1d104961356ba19ee616974d5e0916daa6b6
Brandonlrs27 <43393536+Brandonlrs27@users.noreply.github.com>
2018-11-19 17:23:49 -0800

    Add files via upload

---
+++ E4-3/E4-3/Grafo.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E4_3
+{
+    class Grafo
+    {
+        char[] puntos;//Aqui estaran los puntos
+        bool[,] conexiones;//Aqui las conexiones entre puntos
+
+        public void Crear(char[] arreglo)//Crea puntos y dimensiones de la tabla
+        {
+            puntos = arreglo;
+            conexiones = new bool[arreglo.Length, arreglo.Length];
+        }
+
+        public void AddLink(char punto, char[] uniones)//Busca la posicion de punto indicado y le agrega sus relaciones en la tabla
+        {
+            int x = Array.IndexOf(puntos, punto);
+            for (int i = 0; i < uniones.Length; i++)
+            {
+                conexiones[x, Array.IndexOf(puntos, uniones[i])] = true;
+            }
+        }
+       
+        public void Trayectorias()//Aqui llama a otro metodo para imprimir trayectoria de una manera recursiva
+        {
+            string pasos = "";
+            for (int i = 0; i < puntos.Length; i++)
+            {
+                ImprimirUniones();
+                Caminos(i, pasos, conexiones);
+                Console.ReadKey();
+                Console.Clear();
+            }
+        }
+        public void ImprimirUniones()//Imprime el formato de la tabla 
+        {
+            for (int i = 0; i < puntos.Length; i++)
+            {
+                Console.SetCursorPosition((i * 7) + 5, 0);
+                Console.Write(puntos[i]);
+            }
+
+            for (int i = 0; i < puntos.Length; i++)
+            {
+                Console.SetCursorPosition(0, i + 1);
+                Console.Write(puntos[i]);
+            }
+
+            for (int i = 0; i < puntos.Length; i++)
+            {
+                for (int o = 0; o < puntos.Length; o++)
+                {
+                    Console.SetCursorPosition((i * 7) + 3, o + 1);
+                    Console.Write(conexiones[i, o]);
+                }
+            }
+            Console.SetCursorPosition(0, puntos.Length + 2);
+        }
+        private void Caminos(int vertice, string pasos, bool[,] conexiones)//Teniendo el vertice de incio, una variable que guarde los pasos y una copia de la tabla de realciones
+        {
+            bool ligas = false;//Indicador si el vertice tiene conexiones todavia
+            bool[,] tem;
+            pasos = pasos + " => " + puntos[vertice];//Se agrega los pasos contados al camino
+            for (int i = 0; i < puntos.Length; i++)
+            {//For para pasar por las conexiones
+                if (conexiones[vertice, i] == true)//Si hay una relacion
+                {
+                    tem = conexiones;//Se copia la tabla
+                    ligas = true;//Se indica que hay un camino existente
+                    for (int o = 0; o < puntos.Length; o++)
+                    {//Se modifica para indicar que ya se tomo ese camino
+                        tem[o, vertice] = false;
+                        tem[o, i] = false;
+                    }
+                    Caminos(i, pasos, tem);
+                }
+            }
+            if (!ligas)//Se imprime si ya no hay conexiones
+            {
+                Console.WriteLine(pasos);
+            }
+        }
+    }
+}
---
+++ E4-3/E4-3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E4_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Grafo Ejerci = new Grafo();//Creamos la clase 
+            Ejerci.Crear(new char[] { 'a', 'b', 'c', 'd', 'e', 'f', 'g' });//Le ingresamos los vertices
+            Ejerci.AddLink('a', new char[] { 'a', 'b' });//Y vamos ingresando las relaciones de cada uno
+            Ejerci.AddLink('b', new char[] { 'a', 'c', 'g' });
+            Ejerci.AddLink('c', new char[] { 'b', 'g', 'd' });
+            Ejerci.AddLink('d', new char[] { 'c', 'e', 'f' });
+            Ejerci.AddLink('e', new char[] { 'd', 'f' });
+            Ejerci.AddLink('f', new char[] { 'g', 'd', 'e' });
+            Ejerci.AddLink('g', new char[] { 'b', 'c', 'f' });
+            Ejerci.ImprimirUniones();//Imprimimos nuestra tabla de relaciones
+            Ejerci.Trayectorias();//E imprimimos todos los posibles caminos
+            Console.ReadKey();
+        }
+    }
+}
